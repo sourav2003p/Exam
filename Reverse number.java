5da080cb09fa31bac1c83f9674bfe99583834440
SOURAV PAL <mr.souravjanta@gmail.com>
2025-01-15 16:20:28 +0530

    Reverse number.java

--- Reverse number.java
+++ Reverse number.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
-public class ReverseNumber {
-    public static int reverse(int num) {
-        int reversed = 0;
-        while (num != 0) {
-            int digit = num % 10;
-            reversed = reversed * 10 + digit;
-            num /= 10;
-        }
-        return reversed;
-    }
+public class ReverseNumberPyramid {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number to reverse: ");
-        int num = scanner.nextInt();
-        System.out.println("Reversed number: " + reverse(num));
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        int startNumber = rows * (rows + 1) / 2;
+
+        for(int i = rows; i >= 1; i--) {
+            System.out.format("%" + (rows - i + 1) * 2 + "s", "");
+            for(int j = i; j >= 1; j--) { 
+                System.out.print(startNumber + " ");
+                startNumber--;
+            }
+            System.out.println();
+        }
         scanner.close();
     }
 }
