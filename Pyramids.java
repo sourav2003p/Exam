6fef1747a4a10c87f59e730c7921d71220c34d1e
SOURAV PAL <mr.souravjanta@gmail.com>
2025-01-15 16:21:52 +0530

    Pyramids.java

--- Pyramids.java
+++ Pyramids.java
@@ -1,19 +1,28 @@
-package com.example;
-public class Pyramid {
-    public static void printPyramid(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+public class Main {
 
-    public static void main(String[] args) {
-        printPyramid(5);
+  public static void main(String[] args) {
+    int rows = 5, k = 0, count = 0, count1 = 0;
+
+    for (int i = 1; i <= rows; ++i) {
+      for (int space = 1; space <= rows - i; ++space) {
+        System.out.print("  ");
+        ++count;
+      }
+
+      while (k != 2 * i - 1) {
+        if (count <= rows - 1) {
+          System.out.print((i + k) + " ");
+          ++count;
+        } else {
+          ++count1;
+          System.out.print((i + k - 2 * count1) + " ");
+        }
+
+        ++k;
+      }
+      count1 = count = k = 0;
+
+      System.out.println();
     }
+  }
 }
